812890d66b872fa56279cb23b11e0df2a557080a
seppukku <haratiri@gmail.com>
2018-01-24 15:29:25 -0500

    Final version

--- app/src/main/java/ca/concordia/thierry/a390_ass_1/gradeActivity.java
+++ app/src/main/java/ca/concordia/thierry/a390_ass_1/gradeActivity.java
@@ -1,35 +1,42 @@
+//  gradeActivity.java
+//  Created by: Thierry St-Arnaud, ID 27649460
+//
+//  Displays grades from randomly generated lists of courses and assignments
+
 package ca.concordia.thierry.a390_ass_1;
 
 import android.content.Context;
 import android.database.DataSetObserver;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-import static ca.concordia.thierry.a390_ass_1.Course.generateRandomCourse;
-
 public class gradeActivity extends AppCompatActivity {
 
+    protected ToggleButton toggleLetterButton = null;
     private ArrayList<Course> courseList;
     private ExpandableListView gradeList;
-    private Random rng = new Random();
+    static protected boolean letterGrade = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade);
 
-        gradeList = findViewById(R.id.gradeList);
+        setUpUI();
 
     }
 
@@ -37,18 +44,60 @@ public class gradeActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
 
-        int numberOfCourse = rng.nextInt(5) + 1;
-        courseList = new ArrayList<Course>();
-        for (int i = 0; i < numberOfCourse; i++) {
-            courseList.add(Course.generateRandomCourse());
-        }
+        generateCourseList();
 
         CourseListAdapter gradeListAdapter = new CourseListAdapter(this, courseList);
 
+        configureToggleLetterButton();
+
         gradeList.setAdapter(gradeListAdapter);
+
+    }
+
+        //  Sets up most UI element, including the custom ActionBar with the toggle button
+    private void setUpUI(){
+        Toolbar gradeToolbar = findViewById(R.id.grade_toolbar);
+        setSupportActionBar(gradeToolbar);
+
+        ActionBar gradeActionBar = getSupportActionBar();
+
+        gradeActionBar.setCustomView(R.layout.grade_toolbar_layout);
+
+        gradeActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_CUSTOM);
+        gradeActionBar.setDisplayHomeAsUpEnabled(true);
+
+        gradeList = findViewById(R.id.grade_list);
+    }
+
+        //  Generate between 1 and 5 course to put in the list
+    private void generateCourseList(){
+        Random rng = new Random();
+        int numberOfCourse = rng.nextInt(5) + 1;
+        courseList = new ArrayList<>();
+        for (int i = 0; i < numberOfCourse; i++) {
+            courseList.add(Course.generateRandomCourse());
+        }
+    }
+
+        //  Sets up the toggle_letter_button to change the state of letterGrade
+        //  and refresh the ExpandableListView when required.
+    private void configureToggleLetterButton(){
+        toggleLetterButton =  findViewById(R.id.toggle_letter);
+        letterGrade = false;
+        toggleLetterButton.setChecked(false);
+        toggleLetterButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                letterGrade = isChecked;
+                gradeList.invalidateViews();
+            }
+        });
     }
 }
 
+    //  This inner class will act as the adapter for the grade_list ExpandableListView.
+    //  It extracts information from the list and sublists containing course
+    //  information and creates an appropriate view for the ExpandableListView
 class CourseListAdapter implements ExpandableListAdapter {
     private Context context;
     private ArrayList<Course> courseArrayList;
@@ -75,6 +124,8 @@ class CourseListAdapter implements ExpandableListAdapter {
 
     @Override
     public int getChildrenCount(int groupPosition) {
+        if (courseArrayList.get(groupPosition).getAssignments().size() == 0)
+            return 1;
         return courseArrayList.get(groupPosition).getAssignments().size();
     }
 
@@ -105,38 +156,56 @@ class CourseListAdapter implements ExpandableListAdapter {
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        Course course = (Course) getGroup(groupPosition);
         if (convertView == null) {
-            LayoutInflater inf = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inf.inflate(R.layout.group_items, null);
+            LayoutInflater courseInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = courseInflater.inflate(R.layout.group_items, parent, false);
         }
-        String courseInfo = null;
+
+        Course course = (Course) getGroup(groupPosition);
         ArrayList<Assignment> assignments = course.getAssignments();
-        if (assignments.isEmpty())
-            courseInfo = course.getCourseTitle().trim();
-        else{
+
+        TextView courseGrade = convertView.findViewById(R.id.course_grade);
+        TextView courseTitle = convertView.findViewById(R.id.course_title);
+
+        if (!assignments.isEmpty()){
             int avg = 0;
             for (Assignment assignment: assignments ) {
                 avg += assignment.getAssignmentGrade();
             }
             avg /= assignments.size();
-            courseInfo = course.getCourseTitle().trim() + "     " + avg;
+            if(gradeActivity.letterGrade)
+                courseGrade.setText(convertToLetter(avg));
+            else
+                courseGrade.setText(Integer.toString(avg));
         }
+        else
+            courseGrade.setText("");
 
-        TextView heading = convertView.findViewById(R.id.heading);
-        heading.setText(courseInfo);
+        courseTitle.setText(course.getCourseTitle().trim());
+
         return convertView;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        Assignment courseAssignment = (Assignment) getChild(groupPosition, childPosition);
         if (convertView == null) {
-            LayoutInflater infalInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = infalInflater.inflate(R.layout.child_items, null);
+            LayoutInflater assignmentInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = assignmentInflater.inflate(R.layout.child_items, parent, false);
         }
-        TextView childItem = convertView.findViewById(R.id.childItem);
-        childItem.setText(courseAssignment.getAssignmentTitle().trim());
+        TextView assignmentTitle = convertView.findViewById(R.id.assignment_title);
+        TextView assignmentGrade = convertView.findViewById(R.id.assignment_grade);
+
+        if (courseArrayList.get(groupPosition).getAssignments().size() < 1){
+            assignmentTitle.setText("No assignments for " + courseArrayList.get(groupPosition).getCourseTitle() + "!");
+            assignmentGrade.setText("");
+            return convertView;
+        }
+        Assignment courseAssignment = (Assignment) getChild(groupPosition, childPosition);
+        assignmentTitle.setText(courseAssignment.getAssignmentTitle().trim());
+        if (gradeActivity.letterGrade)
+            assignmentGrade.setText(convertToLetter(courseAssignment.getAssignmentGrade()));
+        else
+            assignmentGrade.setText(Integer.toString(courseAssignment.getAssignmentGrade()));
 
         return convertView;
     }
@@ -174,5 +243,41 @@ class CourseListAdapter implements ExpandableListAdapter {
     @Override
     public long getCombinedGroupId(long groupId) {
         return 0;
+    }
+
+        //  Converts the received number grade to a letter grade using the table
+        //  found at https://alcor.concordia.ca/~higgins/qth/concordia.grades.html
+        //  for reference.
+    private String convertToLetter(int numberGrade){
+        String letterGrade = "F";
+        if (numberGrade <= 100 && numberGrade >= 0){
+            if (numberGrade > 89)
+                letterGrade = "A+";
+            else if (numberGrade > 84)
+                letterGrade = "A";
+            else if (numberGrade > 79)
+                letterGrade = "A-";
+            else if (numberGrade > 76)
+                letterGrade = "B+";
+            else if (numberGrade > 72)
+                letterGrade = "B";
+            else if (numberGrade > 69)
+                letterGrade = "B-";
+            else if (numberGrade > 66)
+                letterGrade = "C+";
+            else if (numberGrade > 62)
+                letterGrade = "C";
+            else if (numberGrade > 59)
+                letterGrade = "C-";
+            else if (numberGrade > 56)
+                letterGrade = "D+";
+            else if (numberGrade > 52)
+                letterGrade = "D";
+            else if (numberGrade > 49)
+                letterGrade = "D-";
+        }
+        else
+            letterGrade = "Invalid grade";
+        return letterGrade;
     }
 }
--- app/src/main/java/ca/concordia/thierry/a390_ass_1/mainActivity.java
+++ app/src/main/java/ca/concordia/thierry/a390_ass_1/mainActivity.java
@@ -1,20 +1,23 @@
+//  mainActivity.java
+//  Created by: Thierry St-Arnaud, ID 27649460
+//
+//  Initial activity to welcome user in the app
+//  Simply displays a button that leads to gradeActivity
+
 package ca.concordia.thierry.a390_ass_1;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class mainActivity extends AppCompatActivity {
 
     private static final String TAG = "mainActivity";
-    //public static final String EXTRA_MESSAGE = "ca.concordia.thierry.a390_ass_1";
-
     protected Button showGradesButton = null;
-    protected TextView buttonTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +27,8 @@ public class mainActivity extends AppCompatActivity {
         setUpUI();
 
         Log.d(TAG, "OnCreate() called.");
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
     }
 
     @Override
@@ -43,20 +38,20 @@ public class mainActivity extends AppCompatActivity {
         Log.d(TAG, "OnDestroy() called.");
     }
 
+    // Sets up most of the UI elements of the activity
     protected void setUpUI() {
-        showGradesButton = (Button) findViewById(R.id.showGradesButton);
-        buttonTest = (TextView) findViewById(R.id.buttonTest);
+        Toolbar mainToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
 
+        showGradesButton = findViewById(R.id.show_grades_button);
         showGradesButton.setOnClickListener(onClickShowGradesButton);
+
     }
 
+    // When show_grades_button is pressed, go to gradeActivity
     private Button.OnClickListener onClickShowGradesButton = new Button.OnClickListener(){
         @Override
         public void onClick(View V){
-            /*if (buttonTest.isShown())
-                buttonTest.setVisibility(View.INVISIBLE);
-            else
-                buttonTest.setVisibility(View.VISIBLE);*/
             Intent GradeIntent = new Intent(getApplicationContext(), gradeActivity.class);
             startActivity(GradeIntent);
         }
